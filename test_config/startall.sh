78ac4008b7ecaad27aecbfa82f5a2f5504de9f90
xuhao <xuhao_xuhao@126.com>
2021-07-24 18:21:18 -0400

    测试配置

--- test_config/startall.sh
+++
@@ -1,6 +0,0 @@
-./../src/redis-server redis-6379.conf &
-./../src/redis-server redis-6380.conf &
-./../src/redis-server redis-6381.conf &
-./../src/redis-server redis-6382.conf &
-./../src/redis-server redis-6383.conf &
-./../src/redis-server redis-6384.conf &
